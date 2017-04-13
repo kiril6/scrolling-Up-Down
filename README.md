45aa8974d3e23f0063f92a3f3462a5275d55d037
MUSALA\kiril.delovski <kiril.delovski@musala.com>
2017-04-13 15:20:12 +0200

    Scrolling detection- works for angular

---
+++ README.md
@@ -0,0 +1,10 @@
+Scroll detection made with javascript and works in Angular 2 which is detecting up/ down scrolling and doing hiding/showing of an element dependent of the direction of scrolling.
+
+The example is done with 2 elements which needed to be shown and hidden.
+
+
+[ngClass] is being used to set animation when classes replaced considering the hide and show of an element when scrolling
+
+If you like this and you found useful you can contribute on my paypal kirilrap@aol.com
+
+Cheers!
---
+++ scroll.ts
@@ -0,0 +1,58 @@
+ private marketsUp: boolean;
+ private filterUp: boolean;
+ private findHeight;
+  
+ 
+ findHide() { 
+    var stickyHeaderTop: any = document.getElementById('live');
+    stickyHeaderTop = stickyHeaderTop.offsetTop;
+    var main: any = document.getElementById('live');
+    var lastScroll = 0;
+    var down = false;
+    var scrollingTop;
+
+    document.getElementById('live').addEventListener('scroll', (e) => {
+
+      let el: any = main;
+
+      if (el.contains(e.target)) {
+        var scroll = main.scrollTop;
+        down = scroll > lastScroll;
+        lastScroll = scroll;
+
+        if (main.scrollTop > 0 && down) { //when scrolling down
+          this.marketsUp = true; hide the element
+          if (main.scrollTop > 95 && down) { //hide after 95px from the top
+            this.filterUp = true; // hide the element
+          }
+        } else {  //when scrolling up
+          this.filterUp = false; // show the element
+          if (scrollingTop) { // if continue scrolling remove the timeout
+            clearTimeout(scrollingTop);
+          }
+          scrollingTop = setTimeout(() => { //timeout for element to be present for some time
+            this.filterUp = true; //hide the element
+          }, 6000);
+
+          this.marketsUp = true; //hide the element
+
+          if (main.scrollTop == 0) {  // when reaches top when scrolling up
+            clearTimeout(scrollingTop);
+            this.marketsUp = false; //show the element
+            this.filterUp = false;  //show the element
+            // scroll = main.scrollTop; scrolling auto to top  
+          }
+        }
+      }
+    }, true);
+  }
+
+  ngAfterViewInit() {
+    var interval = setInterval(() => {
+      var isPresentElement = document.getElementById('live');
+      if (isPresentElement) {
+        this.findHide();
+        clearInterval(interval);
+      }
+    }, 500);
+  }
